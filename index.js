fbf09bdb77ab01927412f313465509ae15d0fd95
unknown <matthieu.oscuro@gmail.com>
2018-03-13 22:44:14 +0100

    udemy 20180313 soir

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
     return (
-        <Header />
+        <View>
+            <Header headerText={'Albums!!'} />
+            <AlbumList />
+        </View>
     );
 };
 
